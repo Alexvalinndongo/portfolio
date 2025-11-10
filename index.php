cabfb0b7a5d1df01a1425d7568c5457d01b16563
Alexvalinndongo <ndongovalin@gmail.com>
2025-11-10 13:51:22 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// index.php
+include('include/header.php');
+include('include/hero.php');
+include('include/about.php');
+include('include/skills.php');
+include('include/projects.php');
+include('include/services.php');
+include('include/experience.php');
+include('include/education.php');
+include('include/contact.php');
+include('include/footer.php');
+?>
